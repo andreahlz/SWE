005d619f0e39c7ce87d32c814825e92a87aa323c
Andrea <andy.fely@gmx.at>
2025-12-06 23:23:59 +0100

    simulation_scripts

---
+++ scripts/make_slurm_job.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+project_root="${script_dir%/scripts}"
+simulation_script="${project_root}/scripts/simulate_short_reads.sh"
+
+Job_name=iss_sim
+cpus=8
+memory=20GB
+time=00:30:00
+
+
+mkdir -p "${project_root}/slurm_scripts"
+job_file="${project_root}/slurm_scripts/slurm_${Job_name}.job"
+
+cat > "${job_file}" <<EOF
+#!/bin/bash
+#
+#SBATCH --job-name=${Job_name}
+#SBATCH --cpus-per-task=${cpus}
+#SBATCH --mem=${memory}
+#SBATCH --mail-type=BEGIN,END,TIME_LIMIT_50,TIME_LIMIT_80,TIME_LIMIT
+#SBATCH --output=%x-%j.out
+#SBATCH --error=%x-%j.err
+#SBATCH --time=${time}
+
+#conda enviroment
+
+#into projct root
+cd ${project_root}
+
+#start simulation
+bash ${simulation_script}
+EOF
+
+echo "Slurm-Jobscript created: ${job_file}"
