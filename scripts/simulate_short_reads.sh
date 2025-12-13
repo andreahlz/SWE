3a68438c81994a08b43a2678242068d8036609c6
Andrea Halasz <andy.fely@gmx.at>
2025-12-13 16:54:53 +0100

    first_tests

--- scripts/simulate_short_reads.sh
+++ scripts/simulate_short_reads.sh
@@ -5,25 +5,24 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 project_root="${script_dir%/scripts}"
 
 #relative paths to project-root
-genome_input_file="${project_root}/data/genomes/mock_gut.fna" #Multifasta file dann
-abundance_input_file="${project_root}/data/abundances/abundance.txt"
+genome_input_file="${project_root}/data/genomes/processed/test_mock_gut_genomes.fna" #Multifasta file dann
+abundance_input_file="${project_root}/data/abundances/test_abbundance.txt"
 output_dir="${project_root}/data/simulated_reads/short_reads"
 file_name="test_01"
 
 #insilco parameters
-n_reads=2400000
+n_reads=20
 error_model="MiSeq"
 seed=42
-cpus=8
+cpus=1
 
 mkdir -p "${output_dir}"
 
 iss generate\
-    --genomes ${genome_input_file}\
-    --abundance_file ${abundance_input_file}\
-    --model ${error_model}\
-    --n_reads ${n_reads}\
-    --seed ${seed}\
-    --cpus ${cpus}\
-    --compress\
-    --output ${output_dir}/${file_name}
+    --genomes "${genome_input_file}" \
+    --abundance_file "${abundance_input_file}" \
+    --model "${error_model}" \
+    --n_reads "${n_reads}" \
+    --seed "${seed}" \
+    --cpus "${cpus}" \
+    --output "${output_dir}/${file_name}"
