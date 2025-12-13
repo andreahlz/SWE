3c8c410995318a836f6b21619a9caee883cb6b55
Andrea Halasz <andy.fely@gmx.at>
2025-12-13 21:28:30 +0100

    config file for relative paths and parameters

--- scripts/simulate_short_reads.sh
+++ scripts/simulate_short_reads.sh
@@ -1,28 +1,20 @@
 #!/bin/bash
 set -euo pipefail
 
+
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 project_root="${script_dir%/scripts}"
 
-#relative paths to project-root
-genome_input_file="${project_root}/data/genomes/processed/test_mock_gut_genomes.fna" #Multifasta file dann
-abundance_input_file="${project_root}/data/abundances/test_abbundance.txt"
-output_dir="${project_root}/data/simulated_reads/short_reads"
-file_name="test_01"
+source "${project_root}/config.env"
 
-#insilco parameters
-n_reads=20
-error_model="MiSeq"
-seed=42
-cpus=1
 
-mkdir -p "${output_dir}"
+mkdir -p "${project_root}/${out_dir_shortreads}"
 
 iss generate\
-    --genomes "${genome_input_file}" \
-    --abundance_file "${abundance_input_file}" \
+    --genomes "${project_root}/${genome_multifasta}" \
+    --abundance_file "${project_root}/${abundance_file}" \
     --model "${error_model}" \
     --n_reads "${n_reads}" \
     --seed "${seed}" \
     --cpus "${cpus}" \
-    --output "${output_dir}/${file_name}"
+    --output "${project_root}/${out_dir_shortreads}/${short_reads_output}"
